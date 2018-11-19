4d1952069f1533c675f86fe652193a385bfa9d81
Patrick <43572773+patryem@users.noreply.github.com>
2018-11-19 12:26:23 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+
+export default class App extends Component {
+  render() {
+    return <Router />
+  }
+}

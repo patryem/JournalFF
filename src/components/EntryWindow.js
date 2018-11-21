d52868afa2b947007f84b26bbbb18fb41e308237
Patrick <43572773+patryem@users.noreply.github.com>
2018-11-21 11:16:30 +0100

    Make one tag at a time clickable

--- src/components/EntryWindow.js
+++ src/components/EntryWindow.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import uid from 'uid'
 
 import Button from './Button'
 import EntryTag from './EntryTag'
@@ -18,10 +19,38 @@ const Wrapper = styled.section`
 export default class EntryWindow extends Component {
   state = {
     tags: [
-      { text: 'work', selected: false },
-      { text: 'sleep', selected: false },
-      { text: 'dance', selected: false }
+      { text: 'work', selected: false, id: uid() },
+      { text: 'sleep', selected: false, id: uid() },
+      { text: 'dance', selected: false, id: uid() }
     ]
+  }
+
+  selectClickedTag(id) {
+    const { tags } = this.state
+    const indexNew = tags.findIndex(tag => tag.id === id)
+    const indexOld = tags.findIndex(tag => tag.selected === true)
+    let newTags
+
+    if (indexOld >= 0) {
+      newTags = [
+        ...tags.slice(0, indexOld),
+        { ...tags[indexOld], selected: false },
+        ...tags.slice(indexOld + 1)
+      ]
+    } else {
+      newTags = tags
+    }
+
+    console.log(newTags)
+    console.log(indexNew)
+    console.log(indexOld)
+    this.setState({
+      tags: [
+        ...newTags.slice(0, indexNew),
+        { ...newTags[indexNew], selected: true },
+        ...newTags.slice(indexNew + 1)
+      ]
+    })
   }
 
   renderTags() {
@@ -29,7 +58,8 @@ export default class EntryWindow extends Component {
       <EntryTag
         text={tag.text}
         selected={tag.selected}
-        onClick={() => console.log('click')}
+        onClick={() => this.selectClickedTag(tag.id)}
+        key={tag.id}
       />
     ))
   }
